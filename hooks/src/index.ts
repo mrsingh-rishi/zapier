816d07ea9452c072e33f73cde9c99e56487b2b45
Rishi Singh <rs3949427@gmail.com>
2024-08-18 13:15:48 +0530

    completed the hooks backend

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,16 +1,39 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.post("/hooks/catch/:userId/:zapId", (req: Request, res: Response) => {
+app.post("/hooks/catch/:userId/:zapId", async (req: Request, res: Response) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
+  const body = req.body || {};
+  try {
+    await prisma.$transaction(async (txn) => {
+      const zaprun = await prisma.zapRun.create({
+        data: {
+          zapId,
+          metadata: body,
+        },
+      });
 
-
-  
+      await prisma.zapRunOutbox.create({
+        data: {
+          zapRunId: zaprun.id,
+        },
+      });
+    });
+    res.status(200).json({ message: "Captured" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to create zapRun" });
+  }
 });
 
 app.listen(port);
