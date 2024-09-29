db5783ebfe01c94f3dd3317c2dbe109b40c4de57
Rishi Singh <rs3949427@gmail.com>
2024-09-29 20:42:07 +0530

    created confirmation offset after processing the messages from kafka

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -14,12 +14,25 @@ async function main() {
   await consumer.subscribe({ topic: TOPIC_NAME });
 
   await consumer.run({
+    autoCommit: false,
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
         partition,
         offset: message.offset,
         value: message.value.toString(),
       });
+
+      await new Promise((r) => setTimeout(r, 5000));
+
+      console.log("SUCCESSFULLY PROCESSED");
+
+      await consumer.commitOffsets([
+        {
+          topic,
+          partition,
+          offset: (parseInt(message.offset) + 1).toString(),
+        },
+      ]);
     },
   });
 }
